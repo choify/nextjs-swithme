a13d186329d8c332d0605dfb4c6327a0ee9e5e75
choify <samuelys@naver.com>
2023-10-06 18:07:44 +0900

    카드 리스트 코드 수정

--- nextjs-swithme-test/src/app/components/RepoDirs.tsx
+++ nextjs-swithme-test/src/app/components/RepoDirs.tsx
@@ -2,6 +2,10 @@
 import React, { useEffect, useState } from 'react';
 import Link from "next/link";
 
+interface Dir{
+    path: string;
+}
+
 interface Content {
     type: string;
     path: string;
@@ -12,34 +16,29 @@ interface RepoDirsProps {
 }
 
 const fetchRepoContents = async (name: string) => {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const response = await fetch(
-        `https://api.github.com/repos/bradtraversy/${name}/contents`
+        `https://api.github.com/repos/bradtraversy/${name}/contents`,
+        {
+            next: {
+                revalidate: 60,
+            },
+        }
     );
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json();
+    const contents = await response.json();
+    return contents;
 };
 
-const RepoDirs: React.FC<RepoDirsProps> = ({ name }) => {
-    const [dirs, setDirs] = useState<Content[]>([]);
-
-    useEffect(() => {
-        fetchRepoContents(name).then(contents => {
-            const dirs = contents.filter((content: Content) => content.type === 'dir');
-            setDirs(dirs);
-        }).catch(error => {
-            console.error("There was an error fetching the repo contents:", error);
-        });
-    }, [name]);
+const RepoDirs: React.FC<RepoDirsProps> = async ({ name }) => {
+    const contents = await fetchRepoContents(name);
+    const dirs = contents.filter((content: Content) => content.type === 'dir');
 
     return (
         <>
             <h3>Directories</h3>
             <ul>
-                {dirs.map((dir) => (
+                {dirs.map((dir: Dir) => (
                     <li key={dir.path}>
                         <Link href={`/code/repos/${name}/${dir.path}`}>{dir.path}</Link>
                     </li>
